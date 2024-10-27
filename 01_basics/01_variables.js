42a6d1a1849b02e2d47df8bd50ea61269e4770f1
Subrat kumar Sahoo <hey.subrat.07@gmail.com>
2024-10-27 18:39:42 +0000

    class 1&2

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,18 @@
+const accountId = 144535
+let accountEmail = "hey.subrat.o7@gmail.com"
+var accountPassword  = "12345"
+accountcity = "cuttack"
+let accountstate;
+// prefer not to use var becasuse of issue in block scope and functional scope
+//accountId = 2 // not aloowed const is constant can't be change
+accountEmail = "ss@gamil"
+accountPassword= "12762"
+accountcity = "bbsrlog"
+
+
+console.log(accountId);
+console.log(accountEmail);
+
+
+
+console.table([accountEmail,accountId,accountPassword,accountcity,accountstate]);
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,35 @@
+"use strict";  //traet all the js code as newer version....
+
+// alert("hello")
+// alert(  3+ 3)   //we are using nodejs not browser
+
+console.log(3 + 3) 
+
+console.log("Hitesh")
+
+//datatypes
+
+let name = "Hitesh"
+
+let age  = 18
+
+let isLoggedIn = false
+
+let  satate ;
+
+let temprature = null ;
+
+//number = 2 to power 53
+//bigint = 
+//string = ""
+//boolean  = true / false
+//null -> its data type and standardlone value
+//undefined
+//symbol -> uniqueness used
+
+
+//object
+
+console.log(typeof null)
+console.log(typeof undefined)
+console.log(typeof "")
