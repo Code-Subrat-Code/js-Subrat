aff04ff294f7c402314211f69b91a6014969fb0a
Subrat kumar Sahoo <hey.subrat.07@gmail.com>
2024-10-27 19:28:51 +0000

    conversions

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,76 @@
+// let score = "Hitesh"
+
+// console.log(typeof (score))
+// console.log(typeof score)
+
+// let x = Number(score)
+// console.log(typeof x)
+// console.log(x)
+
+// *** when we try to convert it into Number***
+//"33" = 33
+//"33abc" = NaN(not a numebr)
+// null = 0
+// undefined = NaN
+// "subrat" = NaN
+//true = 1; false = 0
+
+
+// let isLoggedIn = ""
+
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(typeof booleanIsLoggedIn)
+// console.log( booleanIsLoggedIn)
+
+//1= true ;0 = false
+//"" = false
+//"Subrat" = false
+
+
+// let someNumber = 33
+
+// let stringNumber = String(someNumber)
+// console.log(typeof stringNumber)
+
+
+//*********************************************OPERATION********************************************************** */
+
+let value = 4
+let negValue = -value
+
+//console.log(negValue)
+
+// console.log(2  + 2)
+// console.log(2  - 2)
+// console.log(2  * 2)
+// console.log(2  / 2)
+// console.log(2  % 2)
+// console.log(2  ** 2)
+
+let x ="Hello"
+let  y = " subrat"
+let z = x+y
+// console.log(z)
+
+// console.log("1 "+ 2)
+// console.log(1 +"2")
+
+// console.log("1"+3+3)
+// console.log(1+3+"3")
+
+// console.log(+true)
+// console.log(true+false)
+// console.log(+"")
+
+let num1 ,num2,num3
+
+num1= num2 = num3 = 2+2
+
+let gamecounter =100
+++gamecounter
+console.log(gamecounter)
+
+
+
+
+
